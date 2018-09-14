54f7b210f5781b530d359117f3feb4a39134e152
Brock Whitten <brock@sintaxi.com>
2018-09-14 05:42:48 -0700

    fixes config inheritance

--- platform.js
+++ platform.js
@@ -2,9 +2,7 @@
 var microplatform = require("microplatform")
 var pkg = require("./package.json")
 
-module.exports = microplatform({
-  cmd: "pwa",
-  name: "pwa platform",
+module.exports = microplatform(Object.assign(pkg, {
   platform: "pwa.sh",
   boilerplate: __dirname + "/boilerplate"
-})
+}))
