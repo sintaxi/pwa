513a436c93d7c71b98c1423346c3fbd815c924d0
Brock Whitten <brock@sintaxi.com>
2018-10-15 02:43:49 -0700

    updates microplatform v0.10.0

--- platform.js
+++
@@ -1,8 +0,0 @@
-
-var microplatform = require("microplatform")
-var pkg = require("./package.json")
-
-module.exports = microplatform(Object.assign(pkg, {
-  platform: "pwa.sh",
-  boilerplate: __dirname + "/boilerplate"
-}))
